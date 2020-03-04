a466e023ec061ffb82b73766db255981ccc074d2
jehosafatc <jehosafat.cabrera@gmail.com>
2020-03-03 19:07:41 -0800

    second commit

--- src_ours/nodes_.py
+++ src_ours/nodes_.py
@@ -2,19 +2,14 @@ import numpy as np
 from collections import defaultdict
 from src_ours import *
 
+
 class MCTS_Node(object):
-    def __init__(self, state: planets_environment, parent = None):
+    def __init__(self, state: planets_environment, parent=None):
         self._number_of_visits = 0.
         self._results = defaultdict(int)
         self.state = state
         self.parent = parent
         self.children = []
-
-
-
-
-
-
 
     def expand(self):
         action = self.untried_actions.pop()
@@ -25,3 +20,14 @@ class MCTS_Node(object):
 
     def is_terminal_node(self):
         return self.state.is_game_over()
+
+    def rollout(self):
+        current_rollout_state = self.state
+        while not current_rollout_state.is_game_over():
+            possible_moves = current_rollout_state.get_legal_actions()
+            action = self.rollout_policy(possible_moves)
+            current_rollout_state = current_rollout_state.move(action)
+        return current_rollout_state.game_result
+
+    def rollout_policy(self, possible_moves):
+        return possible_moves[np.random.randint(len(possible_moves))]
---
+++ src_ours/planets_environment.py
@@ -0,0 +1,2 @@
+class planetsenvironemt(object):
+    def __init__(self):
