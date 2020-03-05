bb27102daee3bd487321a17cf91476d8d223a022
Burtony3 <burtonyale+github@gmail.com>
2020-03-05 11:50:41 -0800

    From Scratch Start
    * Moved all old .py files to refs
    * Started sudo .py files in /src
    * Updated readme
    * Added place for spice kernels /data/spk

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # mcts.py
+
+## Folder Structure
+
+### data
+* Spice Kernels
+
+### refs
+* Old python files
+* Tic Tac Toe
+
+### resources
+* Research papers referenced
+
+### src
+* Current source files
--- src/node.py
+++ src/node.py
@@ -1,66 +1,18 @@
+# Importing
 import numpy as np
-from collections import defaultdict
-from sequenceEnv import *
+from sequenceEnv import * as env
 
+# Class Creation
+class node(object):
 
-class MonteCarloTreeSearchNode(object):
-    def __init__(self, state: sequenceState, parent=None):
-        self._number_of_visits = 0.
-        self._results = defaultdict(int)
-        self.state = state
-        self.parent = parent
-        self.children = []
-
-    @property
-    def untried_actions(self):
-        if not hasattr(self, '_untried_actions'):
-            self._untried_actions = self.state.get_legal_actions()
-        return self._untried_actions
-
-    @property
-    def q(self):
-        wins = self._results[self.parent.state.next_to_move]
-        loses = self._results[-1 * self.parent.state.next_to_move]
-        return wins - loses
-
-    @property
-    def n(self):
-        return self._number_of_visits
-
-    def expand(self):
-        # action = self.untried_actions.pop()  # this has to change!!!
-        action = np.random.choice(self.untried_actions)
-        next_state = self.state.move(action)
-        child_node = MonteCarloTreeSearchNode(next_state, parent=self)
-        self.children.append(child_node)
-        return child_node
-
-    def is_terminal_node(self):
-        return self.state.is_game_over()
-
-    def rollout(self):
-        current_rollout_state = self.state
-        while not current_rollout_state.is_game_over():
-            possible_moves = current_rollout_state.get_legal_actions()
-            action = self.rollout_policy(possible_moves)
-            current_rollout_state = current_rollout_state.move(action)
-        return current_rollout_state.game_result
-
-    def backpropagate(self, result):
-        self._number_of_visits += 1.
-        self._results[result] += 1.
-        if self.parent:
-            self.parent.backpropagate(result)
-
-    def is_fully_expanded(self):
-        return len(self.untried_actions) == 0
-
-    def best_child(self, c_param=1.4):
-        choices_weights = [
-            (c.q / (c.n)) + c_param * np.sqrt((2 * np.log(self.n) / (c.n)))
-            for c in self.children
-        ]
-        return self.children[np.argmax(choices_weights)]
-
-    def rollout_policy(self, possible_moves):
-        return possible_moves[np.random.randint(len(possible_moves))]
+    # Blank Node Creation
+    def __init__(self, state, parent=0, dvAcc = 0, vInfIn = np.zeros([3 1]), vInfOut):
+        self.n = 0              # Visits in UCB1 Function
+        self.cost = None        # Estimated Reward in UCB1 Function (if -1 = dead node)
+        self.dvAcc = dvAcc      # Δv used up to that point
+        self.parent = parent    # Establishes Parent above (Single Scalar of IDs)
+        self.child = None       # Children Below (Array of IDs)
+        self.state = state      # Either NAIF ID String or Epoch
+        self.isLeaf = True      # Is end of branch
+        self.vInfIn = vInfIn    # V∞ into CURRET planet/epoch pair
+        self.vInfOut = vInfOut  # V∞ out of PREVIOUS planet/epoch pair
