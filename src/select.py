b878511cd5ecf48709e1750b8d9f6269d31d2d62
Rohan Patel <56667817+rohanpatel747@users.noreply.github.com>
2020-03-06 22:51:27 -0800

    i.c. select py sorta sudo code but also some real code

---
+++ src/select.py
@@ -0,0 +1,37 @@
+import math
+
+# Selection
+def select():
+    cp = 1/sqrt(2.0)
+
+    id = 0
+    while node[id].children != None:
+
+        ucb1 = []
+        # Checks Node's children
+        for i in range(len(node[id].children)):
+            X = node[node[id].children[i]].cost
+            N = node[id].n
+            n = node[node[id].children[i]].n
+
+            if n == 0:
+                # Sets UCB to inf if not been visited yet
+                ucb1.append(inf)
+                id = node[id].children[i]
+                # Saves first child that has inf value (left to right arb. selection from video)
+                break
+            else:
+                ucb1.append(X + cp*math.sqrt(math.log1p(N)/n))
+
+        # Find UCB w/ max value
+        indexChild = ucb1.index(max(ucb1))
+
+        # Set the id to this child's index in the Node list
+        id = node[id].children[indexChild]
+
+    if node[id].n == 0:
+        expandBool = False
+    else:
+        expandBool = True
+
+    return id , expandBool
